bf1129bd2c9e50142b4fc3157b13746a5f211c40
ledangkhanhhy <ledangkhanhhy@gmail.com>
2026-06-15 22:03:05 +0700

    feat: add filterBrandId prop to BrandContainer in BrandPage

--- src/pages/brands/BrandPage.tsx
+++ src/pages/brands/BrandPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import BrandContainer from "../../features/brands/brandContainer/brandContainerAll";
 
 const BrandPage: React.FC = () => {
-  return <BrandContainer/>;
+  return <BrandContainer filterBrandId=""/>;
 };
 
 export default BrandPage;
