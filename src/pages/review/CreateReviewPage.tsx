ed86a7cf29796a915a046ba7d8e64a80564294fc
ledangkhanhhy <ledangkhanhhy@gmail.com>
2026-06-20 21:59:34 +0700

    feat(review): remove CreateReviewPage component as part of review feature refactoring

--- src/pages/review/CreateReviewPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CreateFormReviewContainer from "../../features/review/reviewContainer/createFormReviewContainer";
-
-const CreateReviewPage: React.FC = () => {
-  return <CreateFormReviewContainer />;
-};
-
-export default CreateReviewPage;
