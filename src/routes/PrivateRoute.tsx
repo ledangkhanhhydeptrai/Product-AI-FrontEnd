9ca152f589d98f934944b6c564118721b243229f
ledangkhanhhy <ledangkhanhhy@gmail.com>
2026-07-13 16:51:51 +0700

    refactor(PrivateRoute): replace token check with isAuthenticated from state

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,14 +1,14 @@
 import { Navigate } from "react-router-dom";
-
+import { useAppSelector } from "../hooks/useAppSelector";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function PrivateRoute({ children }: Props) {
-  const token = localStorage.getItem("token");
-  
-  if (!token) {
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
+
+  if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
 
